11f4aedec203cff81bfc934ccc632592267c2759
Joe Zurner <70181039+jzurner1@users.noreply.github.com>
2024-03-23 23:44:40 -0400

    Update BasicChaseEnemy1.cs

--- BasicChaseEnemy1.cs
+++ BasicChaseEnemy1.cs
@@ -1,37 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicChaseAI : BaseEnemy
 {
+    private Rigidbody2D rb;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody2D component missing from object.");
+        }
+
         // currently this script inherits from another script that has a MovementSpeed value. If you don't want to inherit, uncomment the next line and make sure you change BaseEnemy (in the class) to MonoBehaviour
         // float MovementSpeed = 5f;
     }
 
     void Update()
     {
-        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");  // get the player
+        AlignRotationWithPlayer();
+        RepelFromOtherEntities();
+        ChasePlayer();
+    }
+
+    void AlignRotationWithPlayer()  // rotate to match player
+    {
+        transform.rotation = Quaternion.Euler(0, 0, PlayerMovement.CurrentRotationZ);
+    }
+
+    void ChasePlayer()  // locate player and call MoveTowards
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
         if (playerObject != null)
         {
-            Vector3 playerPosition = playerObject.transform.position;
-            Vector3 direction = (playerPosition - transform.position).normalized;  // vector in player's direction with magnitude 1
-
-            Rigidbody2D rb = GetComponent<Rigidbody2D>();
-
-            if (rb != null)
-            {
-                rb.velocity = new Vector2(direction.x, direction.y) * MovementSpeed;
-            }
-            else
-            {
-                Debug.LogError("This object does not have a Rigidbody2D!");
-            }
+            MoveTowards(playerObject.transform.position);
         }
         else
         {
-            Debug.Log("Could not locate player! Make sure they have the tag 'Player'!");
+            Debug.LogWarning("Could not locate player! Make sure they have the tag 'Player'.");
         }
+    }
+
+    void MoveTowards(Vector3 targetPosition)  // actual movement
+    {
+        if (rb == null) return;
+
+        Vector3 direction = (targetPosition - transform.position).normalized;
+        rb.velocity = new Vector2(direction.x, direction.y) * MovementSpeed;
     }
 }
